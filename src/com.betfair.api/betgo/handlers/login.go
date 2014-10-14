338d5e9bda6cbfb6be302a8704180c59475508c5
doowad <dasmeet.doowa@gmail.com>
2014-10-14 17:25:09 +0100

    login impl

---
+++ src/com.betfair.api/betgo/handlers/login.go
@@ -0,0 +1,8 @@
+package handlers
+
+import (
+	"net/http"
+)
+func Login (w http.ResponseWriter, r *http.Request) {
+
+}
--- src/com.betfair.api/betgo/server/server.go
+++ src/com.betfair.api/betgo/server/server.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-"net/http"
-"fmt"
-"strings"
+	"net/http"
+	"fmt"
+	"strings"
+	"com.betfair.api/betgo/handlers"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,8 +17,14 @@ func shouthelloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello %s!", strings.ToUpper(remPartOfURL))
 }
 
+func loginCall(w http.ResponseWriter, r *http.Request) {
+	handlers.Login(w,r)
+
+}
+
 func main() {
 	http.HandleFunc("/hello/", helloHandler)
 	http.HandleFunc("/shouthello/", shouthelloHandler)
+	http.HandleFunc("/login", loginCall)
 	http.ListenAndServe("localhost:9000", nil)
 }
